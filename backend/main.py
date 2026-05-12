e0fe9a1b89a2162af35c75e4e14016b2553ad6bd
MusfirahWasim <musfirahwaseem1@gmail.com>
2026-05-12 09:20:21 +0500

    origin change

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://taskitt.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
